23733ff34c01a0318fd8e35e8579ecae3b2e32cf
Aditya Ranjan <57545175+aditya393@users.noreply.github.com>
2023-03-05 23:26:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from "./components/pages/Home"
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home></Home>} />
+          </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
